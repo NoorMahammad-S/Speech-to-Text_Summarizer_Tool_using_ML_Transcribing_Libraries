d519259fd07ee2821505965e3c5dedec4118cc46
Your Name <you@example.com>
2023-11-27 09:25:29 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+import os
 import speech_recognition as sr
 from gensim.summarization import summarize
 
+
 def transcribe_audio(audio_path):
+    if not os.path.exists(audio_path):
+        print(f"Error: Audio file not found at {audio_path}")
+        return None
+
     recognizer = sr.Recognizer()
 
     with sr.AudioFile(audio_path) as source:
-        audio_data = recognizer.record(source)
+        try:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+            return text
+        except sr.UnknownValueError:
+            print("Error: Google Speech Recognition could not understand the audio.")
+        except sr.RequestError as e:
+            print(f"Error: Could not request results from Google Speech Recognition service; {e}")
 
-    try:
-        text = recognizer.recognize_google(audio_data)
-        return text
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand the audio.")
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+    return None
+
 
 def summarize_text(text):
-    summary = summarize(text)
-    return summary
+    if text:
+        summary = summarize(text)
+        return summary
+    else:
+        return None
+
 
 def main():
     audio_path = "path/to/your/audio/file.wav"
@@ -29,13 +41,17 @@ def main():
         # Step 2: Summarize the transcribed text
         summarized_text = summarize_text(transcribed_text)
 
-        # Step 3: Display the results
-        print("Transcribed Text:")
-        print(transcribed_text)
-        print("\nSummarized Text:")
-        print(summarized_text)
+        if summarized_text:
+            # Step 3: Display the results
+            print("Transcribed Text:")
+            print(transcribed_text)
+            print("\nSummarized Text:")
+            print(summarized_text)
+        else:
+            print("Failed to summarize the transcribed text.")
     else:
         print("Failed to transcribe the audio.")
+
 
 if __name__ == "__main__":
     main()
